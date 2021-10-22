c44d2eaa6277d1ec0b31061fa9504e0f705a3f9a
Clara Mayorga <mayorgaclara@gmail.com>
2021-10-22 01:25:23 -0300

    Unit one finished

---
+++ src/main/java/com.alkemy.icons.icons/entity/ContinenteEntity.java
@@ -0,0 +1,23 @@
+package com.alkemy.icons.icons.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "continente")
+@Getter
+@Setter
+public class ContinenteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String imagen;
+
+    private String denominacion;
+
+}
