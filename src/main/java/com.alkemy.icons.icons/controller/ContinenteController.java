4076f9e9fbf5370d4a7e93c1ad08c2baaa953ae7
Clara Mayorga <mayorgaclara@gmail.com>
2021-10-27 23:39:40 -0300

    create & save continent completed

---
+++ src/main/java/com.alkemy.icons.icons/controller/ContinenteController.java
@@ -0,0 +1,39 @@
+package com.alkemy.icons.icons.controller;
+
+import com.alkemy.icons.icons.dto.ContinenteDTO;
+import com.alkemy.icons.icons.service.ContinenteService;
+import com.alkemy.icons.icons.service.impl.ContinenteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*Para definir que es un controlador*/
+@RestController
+/*Para definir la URL - sugerido en plural*/
+@RequestMapping("continentes")
+
+public class ContinenteController {
+
+    @Autowired
+    private ContinenteService continenteService;
+
+    @GetMapping
+    public ResponseEntity<List<ContinenteDTO>> getAll() {
+        List<ContinenteDTO> continentes = continenteService.getAllContinentes();
+        return ResponseEntity.ok().body(continentes);
+
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ContinenteDTO> save(@RequestBody ContinenteDTO continente) {
+        ContinenteDTO continenteGuardado = continenteService.save(continente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(continenteGuardado);
+
+
+    }
+    
+}
---
+++ src/main/java/com.alkemy.icons.icons/service/impl/ContinenteServiceImpl.java
@@ -0,0 +1,39 @@
+package com.alkemy.icons.icons.service.impl;
+
+import com.alkemy.icons.icons.dto.ContinenteDTO;
+import com.alkemy.icons.icons.entity.ContinenteEntity;
+import com.alkemy.icons.icons.mapper.ContinenteMapper;
+import com.alkemy.icons.icons.repository.ContinenteRepository;
+import com.alkemy.icons.icons.service.ContinenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContinenteServiceImpl implements ContinenteService {
+
+    @Autowired
+    private ContinenteMapper continenteMapper;
+    @Autowired
+    private ContinenteRepository continenteRepository;
+
+
+    public ContinenteDTO save(ContinenteDTO dto) {
+        // Operacion de guardado en 3 pasos:
+        // Lo convierto a Entity
+        ContinenteEntity entity = continenteMapper.continenteDTO2Entity(dto);
+        //Lo guardo y me devuelve la entidad guardada
+        ContinenteEntity entitySaved = continenteRepository.save(entity);
+        //Convierto la entidad guardada en un dto
+        ContinenteDTO result = continenteMapper.continenteEntity2DTO(entitySaved);
+        return result;
+    }
+
+    public List<ContinenteDTO> getAllContinentes() {
+        List<ContinenteEntity> entities = continenteRepository.findAll();
+        List<ContinenteDTO> result = continenteMapper.continenteEntityList2DTOList(entities);
+        return result;
+    }
+
+}
